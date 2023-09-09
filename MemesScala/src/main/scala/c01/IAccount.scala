8849ac11a76b0e3bb1e7677ef1e94e4211695ff7
FrancoGonzalezL <franco.gonzalez.l@ug.uchile.cl>
2023-09-09 12:33:49 -0300

    (memes)

--- MemesScala/src/main/scala/c01/IAccount.scala
+++ MemesScala/src/main/scala/c01/IAccount.scala
@@ -22,3 +22,25 @@ object JAccount extends IAccount {
         true
     }
 }
+class Account extends IAccount {
+    private var balance: Int = 10000
+    private val currency: String = "CLP"
+
+    def getBalance(): Int = balance
+    def withdraw(n: Int): Boolean = {
+        if(balance>=n){
+            balance -= n
+            true
+        }else false
+    }
+    def deposit(n: Int): Boolean = {
+        balance += n
+        true
+    }
+}
+
+class VIPAccount extends Account {
+    override def withdraw(n: Int): Boolean = {
+        withdraw(n/2)
+    }
+}
--- MemesScala/src/main/scala/c01/Main.scala
+++ MemesScala/src/main/scala/c01/Main.scala
@@ -2,13 +2,8 @@ package c01
 
 object Main {
     def main(args: Array[String]): Unit = {
-
-        val x: IAccount = JAccount
-
-        x.withdraw(10)
-        println(JAccount.getBalance())
-        println(x.getBalance())
-        println(x == JAccount)
+        val x: IAccount = new VIPAccount
+        println(s"${x.getBalance()}")
 
     }
 }
